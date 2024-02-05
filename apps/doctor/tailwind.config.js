02e0567b086e77057312e542e4b687a1d4b3148a
hte88 <olivier.marion88@gmail.com>
2024-02-05 15:05:11 +0100

    feat: apps nuxt

---
+++ apps/doctor/tailwind.config.js
@@ -0,0 +1,10 @@
+/* module.exports = {
+  plugins: [require('daisyui')],
+};
+ */
+//module.exports = require('@myturborepo/configs/tailwind/tailwind.config')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('@myturborepo/configs/tailwind/tailwind.config')],
+}
